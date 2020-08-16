3ff86de2b01e2d3e378da0032f550eca696866d2
rrudiuk <rus.barik@gmail.com>
2020-08-16 12:24:04 +0300

    Added a few methods to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,6 +14,31 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+    def click_element(self, how, what):
+        try:
+            self.browser.find_element(how, what).click()
+        except NoSuchElementException:
+            return False
+        return True
+
+    def count_elements(self, how, what):
+        try:
+            return len(self.browser.find_elements(how, what))
+        except NoSuchElementException:
+            return 0
+
+    def locate_element(self, how, what):
+        try:
+            return self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+
+    def locate_elements(self, how, what):
+        try:
+            return self.browser.find_elements(how, what)
+        except NoSuchElementException:
+            return False
+
     def is_disappeared(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException).\
@@ -37,6 +62,14 @@ class BasePage:
             return True
 
         return False
+
+    def get_text(self, how, what, encoding=None):
+
+        try:
+            text = self.browser.find_element(how, what).text
+        except NoSuchElementException:
+            return False
+        return text.encode(encoding) if encoding else text
 
     def open(self):
         self.browser.get(self.url)
